cc8a83ed46fba4564b33dbc22b83bb6024d4c529
chernysn <136198764+chernysn@users.noreply.github.com>
2023-10-14 12:24:27 -0400

    first commit

---
+++ app/nc_tutorials/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tutorials.urls')),
+    path('', include('users.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
